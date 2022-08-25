b019f0744508c1254fe77d59c2b5f962b968f8e0
witwait <orphapiccirillirrx60@gmail.com>
2022-08-25 08:10:33 -0700

    leaf_node_split_and_insert

--- db.c
+++ db.c
@@ -474,6 +474,50 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table)
   }
 }
 
+uint32_t get_unused_page_num(Pager* pager){
+  return pager->num_pages;
+}
+
+const LEAF_NODE_RIGHT_SPLIT_COUNT = (LEAF_NODE_MAX_CELLS+1)/2;
+const LEAF_NODE_LEFT_SPLIT_COUNT = LEAF_NODE_MAX_CELLS+1 - LEAF_NODE_RIGHT_SPLIT_COUNT;
+
+void leaf_node_split_and_insert(Cursor* cursor,uint32_t key,Row* value){
+  void* old_node = get_page(cursor->table->pager,cursor->cell_num);
+  uint32_t new_page_num  = get_unused_page_num(cursor->table->pager);
+  void* new_node = get_page(cursor->table->pager,new_page_num);
+  initialize_leaf_node(new_node);
+
+  for(int32_t i = LEAF_NODE_MAX_CELLS; i>=0;i--){
+    void* destination_node;
+    if(i>= LEAF_NODE_LEFT_SPLIT_COUNT){
+      destination_node = new_node;
+    } else {
+      destination_node = old_node;
+    }
+    uint32_t index_within_node = i % LEAF_NODE_LEFT_SPLIT_COUNT;
+    void* destination = leaf_node_cell(destination_node,index_within_node);
+
+    if(i==cursor->cell_num){
+      serialize_row(value,destination);
+    } else if( i > cursor->cell_num){
+      memcpy(destination,leaf_node_cell(old_node,i-1),LEAF_NODE_CELL_SIZE);
+    } else{
+      memcpy(destination,leaf_node_cell(old_node,i),LEAF_NODE_CELL_SIZE);
+    }
+  }
+  *(leaf_node_num_cells(old_node)) = LEAF_NODE_LEFT_SPLIT_COUNT;
+  *(leaf_node_num_cells(new_node)) = LEAF_NODE_RIGHT_SPLIT_COUNT;
+
+  if(is_node_root(old_node)){
+    return create_new_root(cursor->table,new_page_num);
+  } else {
+    printf("Need to implement updating parent after split.\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+
+
 void leaf_node_insert(Cursor *cursor, uint32_t key, Row *value)
 {
   void *node = get_page(cursor->table->pager, cursor->page_num);
@@ -481,8 +525,8 @@ void leaf_node_insert(Cursor *cursor, uint32_t key, Row *value)
   uint32_t num_cells = *leaf_node_num_cells(node);
   if (num_cells >= LEAF_NODE_MAX_CELLS)
   {
-    printf("Need to implement splitting a leaf node.\n");
-    exit(EXIT_FAILURE);
+    leaf_node_split_and_insert(cursor,key,value);
+    return;
   }
   if (cursor->cell_num < num_cells)
   {
@@ -552,10 +596,6 @@ ExecuteResult execute_insert(Statement *statement, Table *table)
 {
   void *node = get_page(table->pager, table->root_page_num);
   uint32_t num_cells = *leaf_node_num_cells(node);
-  if (num_cells >= LEAF_NODE_MAX_CELLS)
-  {
-    return EXECUTE_TABLE_FULL;
-  }
 
   Row *row_to_insert = &(statement->row_to_insert);
   uint32_t key_to_insert = row_to_insert->id;
